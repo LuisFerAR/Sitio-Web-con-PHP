e2db0b1ecee735b786b3dfaad534a077e68eb008
Luis Fernando Aranda Ramos <luis.fernando.aranda.ramos@gmail.com>
2023-12-17 17:58:14 -0500

    17/12/23 6:00pm

---
+++ admin/secciones/servicios/index.php
@@ -0,0 +1,44 @@
+<?php  include("../../templates/header.php");  ?>
+
+Listar servicios
+
+<div class="card">
+    <div class="card-header">
+        <a name="" id="" class="btn btn-primary" href="crear.php" role="button">Agregar registros</a>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive-sm">
+            <table class="table table">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Icono</th>
+                        <th scope="col">Titulo</th>
+                        <th scope="col">Descripcion</th>
+                        <th scope="col">Accion</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="">
+                        <td>1</td>
+                        <td>fa-book</td>
+                        <td>tutorias</td>
+                        <td>servicios de tutoria para programacion</td>
+                        <td>Editar|Eliminar</td>
+                    </tr>
+                    <tr class="">
+                        <td>1</td>
+                        <td>fa-book</td>
+                        <td>tutorias</td>
+                        <td>servicios de tutoria para programacion</td>
+                        <td>Editar|Eliminar</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="card-footer text-muted"></div>
+</div>
+
+
+<?php  include("../../templates/footer.php");  ?>
